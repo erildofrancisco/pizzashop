34592293f1b00790df5a54417e5f4dd2c157b805
erildofrancisco <erildofranciscojs@gmail.com>
2026-01-22 11:26:07 +0100

    chore: add tailwind and shadcnui

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,12 @@
+import { Button } from "./components/ui/button";
 
 export function App() {
 
 
   return (
-    <h1>hello</h1>
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
   )
 }
 
